1266840e6d680d24922d84e3fb95e69a72326cb0
Tofag <103821367+Tofag-py@users.noreply.github.com>
2022-04-26 06:41:22 +0100

    Update main.py

--- POC/main.py
+++ POC/main.py
@@ -9,25 +9,6 @@ def base_url(endpoint, x):
     return per_page.json()
 
 
-def get_pages(response):
-    return response['pages']
-
-
-def parse_data(response):
-    charlist = []
-    for item in response['result']:
-        char = {
-            'name': item['data'],
-            'no_ep': len(item['id']),
-        }
-
-        charlist.append(char)
-        return charlist
-
-
 data = base_url(endpoint, 20)
-print(data)
 
-get_pages = endpoint + f'?page={y}'
-per_page = get_pages(endpoint, 2)
-print(parse_data(per_page))
+pprint(data)
