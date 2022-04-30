c86d5b0665abf2752374d89cc0af8deec262f55a
Tofag <103821367+Tofag-py@users.noreply.github.com>
2022-05-01 00:50:59 +0100

    Update main.py

--- POC/main.py
+++ POC/main.py
@@ -1,14 +1,24 @@
 import requests
-from pprint import pprint
-
-endpoint = "https://informed-data-challenge.netlify.app/api/breweries"
+#from pprint import pprint
 
 
-def base_url(endpoint, x):
-    per_page = requests.get(endpoint + f'?per_page={x}')
-    return per_page.json()
+page = 0
+all_data_entries = []
+per_page = 20
 
+while True:
+    print("----------")
+    endpoint = f"https://informed-data-challenge.netlify.app/api/breweries?page={page}&per_page={per_page}"
+    print("Requesting data..." + endpoint)
+    #payload = {'page': page, 'per_page': 20}
+    r = requests.get(url=endpoint).json()
 
-data = base_url(endpoint, 20)
-
-pprint(data)
+    print(r)
+    try:
+        result = r['data'][0]
+    except IndexError:
+        # We found an error
+        print("Extraction Completed, No more data.....")
+        break
+    all_data_entries.extend(r['data'])
+    page = page + 1
