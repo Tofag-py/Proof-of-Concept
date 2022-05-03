be582f54a33adfb9e2f83c94cb8daea04f7d8b64
Tofag <103821367+Tofag-py@users.noreply.github.com>
2022-05-03 12:23:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from trial import data_model
+
+
+# import os
+# import psycopg2
+
+def get_all_data():
+    page = 0
+    all_data_entries = []
+    per_page = 20
+
+    while True:
+        print("----------")
+        endpoint = f"https://informed-data-challenge.netlify.app/api/breweries?page={page}&per_page={per_page}"
+        print("Requesting data..." + endpoint)
+        # payload = {'page': page, 'per_page': 20}
+        r = requests.get(url=endpoint).json()
+
+        print(r)
+        try:
+            result = r['data'][0]
+        except IndexError:
+            # We found an error
+            print("Extraction Completed, No more data.....")
+            break
+        all_data_entries.extend(r['data'])
+        page = page + 1
+        
+    data = all_data_entries
+    return data
+
+
+data_model(get_all_data())
--- trial.py
+++ trial.py
@@ -1,54 +1,56 @@
-import os
 import psycopg2
-import main
-
-DB_HOST = "ec2-34-247-72-29.eu-west-1.compute.amazonaws.com"
-DB_NAME = "dad1sf5fvfqo64"
-DB_USER = "cdxpreinbalvbw"
-DB_PASS = "8b794319be9f1d93f7067a2544843ef4a466b4f9e1c0461f13185d43be2f9550"
-
-# DATABASE_URL = os.environ['postgres://pglpycxcttzwwv:846f5e5b4a51c20a95c81115b2eb5fc739429f2f3b10326e2a84fb28661d1da0@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d9fvhj9uj2modd']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-conn = None
-cur = None
-try:
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-    cur = conn.cursor()
-
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS Proof_of_Concept (
-        ID text PRIMARY KEY, 
-        Name VARCHAR, 
-        brewery_type VARCHAR, 
-        street VARCHAR, 
-        address_2 VARCHAR, 
-        address_3 VARCHAR, 
-        city VARCHAR, 
-        state VARCHAR, 
-        county_province VARCHAR, 
-        postal_code VARCHAR, 
-        website_url VARCHAR, 
-        phone INT, 
-        country VARCHAR, 
-        longitude decimal, 
-        latitude decimal,
-        tags VARCHAR, 
-        ratings DECIMAL, 
-        number_of_ratings INT);"""
-    )
-
-    conn.commit()
-
-    myDict = main.r
-
-    values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in myDict.values())
-    cur.execute("INSERT INTO %s ( %s ) values (%s);" % ('Proof_of_Concept', values))
+from db import dbd
 
 
-except Exception as error:
-    print(error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
+def data_model(data):
+    conn = None
+    cur = None
+    try:
+        conn = dbd()
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        cur.execute('SELECT %s as connected;', ('connection to post was success',))
+        print(cur.fetchone())
+        if cur.execute('DROP TABLE IF EXISTS Brewery'):
+            print("Table already exists")
+        else:
+            cur.execute(
+                """CREATE TABLE Brewery(
+                    ID_db SERIAL NOT NULL,
+                    id VARCHAR(128), 
+                    name VARCHAR(128), 
+                    brewery_type VARCHAR(128), 
+                    street VARCHAR(128), 
+                    address_2 VARCHAR(128), 
+                    address_3 VARCHAR(128), 
+                    city VARCHAR(128), 
+                    state VARCHAR(128), 
+                    county_province VARCHAR(128), 
+                    postal_code VARCHAR(128), 
+                    website_url VARCHAR(128), 
+                    phone VARCHAR(128), 
+                    country VARCHAR(128), 
+                    longitude VARCHAR(128), 
+                    latitude VARCHAR(128),
+                    tags VARCHAR(128), 
+                    ratings DECIMAL, 
+                    number_of_ratings VARCHAR(128))"""
+            )
+            # converting list of dict to list of turple
+
+            data_tuples = [tuple(datum.values()) for datum in data]
+            values_template = ','.join(['%s'] * len(data_tuples))
+
+            cur.execute("INSERT INTO Brewery(id, name, brewery_type, street, address_2, " \
+                        "address_3, city, state, county_province, postal_code, website_url, phone, country, " \
+                        "longitude, latitude, tags, ratings, number_of_ratings) VALUES {}".format(values_template),
+                        data_tuples)
+
+    except Exception as error:
+        print(error)
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
